c3d337f61b2a4081c5a08c4383bf0eb8b81f43b7
stonecoldcrazymusic-dotcom <stonecoldcrazymusic@gmail.com>
2025-11-18 21:07:30 +0100

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "Stone Cold Crazy";
+export const SITE_DESCRIPTION = "Stone Cold Crazy | Music | New Media | Digital Culture | Consultancy | Bookings | Productions";
